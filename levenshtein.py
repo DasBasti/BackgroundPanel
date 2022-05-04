225faa460503c5aeb6d23f6ed003183f4d6d1e28
Bastian Neumann <neumann.bastian@gmail.com>
2022-05-04 19:10:09 +0100

    ADD: Levenshtein distance for color guessing

---
+++ levenshtein.py
@@ -0,0 +1,197 @@
+
+css3_colors=[
+"AliceBlue",
+"AntiqueWhite",
+"Aqua",
+"Aquamarine",
+"Azure",
+"Beige",
+"Bisque",
+"Black",
+"BlanchedAlmond",
+"Blue",
+"BlueViolet",
+"Brown",
+"BurlyWood",
+"CadetBlue",
+"Chartreuse",
+"Chocolate",
+"Coral",
+"CornflowerBlue",
+"Cornsilk",
+"Crimson",
+"Cyan",
+"DarkBlue",
+"DarkCyan",
+"DarkGoldenRod",
+"DarkGray",
+"DarkGrey",
+"DarkGreen",
+"DarkKhaki",
+"DarkMagenta",
+"DarkOliveGreen",
+"DarkOrange",
+"DarkOrchid",
+"DarkRed",
+"DarkSalmon",
+"DarkSeaGreen",
+"DarkSlateBlue",
+"DarkSlateGray",
+"DarkSlateGrey",
+"DarkTurquoise",
+"DarkViolet",
+"DeepPink",
+"DeepSkyBlue",
+"DimGray",
+"DimGrey",
+"DodgerBlue",
+"FireBrick",
+"FloralWhite",
+"ForestGreen",
+"Fuchsia",
+"Gainsboro",
+"GhostWhite",
+"Gold",
+"GoldenRod",
+"Gray",
+"Grey",
+"Green",
+"GreenYellow",
+"HoneyDew",
+"HotPink",
+"IndianRed",
+"Indigo",
+"Ivory",
+"Khaki",
+"Lavender",
+"LavenderBlush",
+"LawnGreen",
+"LemonChiffon",
+"LightBlue",
+"LightCoral",
+"LightCyan",
+"LightGoldenRodYellow",
+"LightGray",
+"LightGrey",
+"LightGreen",
+"LightPink",
+"LightSalmon",
+"LightSeaGreen",
+"LightSkyBlue",
+"LightSlateGray",
+"LightSlateGrey",
+"LightSteelBlue",
+"LightYellow",
+"Lime",
+"LimeGreen",
+"Linen",
+"Magenta",
+"Maroon",
+"MediumAquaMarine",
+"MediumBlue",
+"MediumOrchid",
+"MediumPurple",
+"MediumSeaGreen",
+"MediumSlateBlue",
+"MediumSpringGreen",
+"MediumTurquoise",
+"MediumVioletRed",
+"MidnightBlue",
+"MintCream",
+"MistyRose",
+"Moccasin",
+"NavajoWhite",
+"Navy",
+"OldLace",
+"Olive",
+"OliveDrab",
+"Orange",
+"OrangeRed",
+"Orchid",
+"PaleGoldenRod",
+"PaleGreen",
+"PaleTurquoise",
+"PaleVioletRed",
+"PapayaWhip",
+"PeachPuff",
+"Peru",
+"Pink",
+"Plum",
+"PowderBlue",
+"Purple",
+"RebeccaPurple",
+"Red",
+"RosyBrown",
+"RoyalBlue",
+"SaddleBrown",
+"Salmon",
+"SandyBrown",
+"SeaGreen",
+"SeaShell",
+"Sienna",
+"Silver",
+"SkyBlue",
+"SlateBlue",
+"SlateGray",
+"SlateGrey",
+"Snow",
+"SpringGreen",
+"SteelBlue",
+"Tan",
+"Teal",
+"Thistle",
+"Tomato",
+"Turquoise",
+"Violet",
+"Wheat",
+"White",
+"WhiteSmoke",
+"Yellow",
+"YellowGreen",
+]
+
+import numpy as np
+def levenshtein_ratio_and_distance(s, t, ratio_calc = False):
+    """ levenshtein_ratio_and_distance:
+        Calculates levenshtein distance between two strings.
+        If ratio_calc = True, the function computes the
+        levenshtein distance ratio of similarity between two strings
+        For all i and j, distance[i,j] will contain the Levenshtein
+        distance between the first i characters of s and the
+        first j characters of t
+    """
+    # Initialize matrix of zeros
+    rows = len(s)+1
+    cols = len(t)+1
+    distance = np.zeros((rows,cols),dtype = int)
+
+    # Populate matrix of zeros with the indeces of each character of both strings
+    for i in range(1, rows):
+        for k in range(1,cols):
+            distance[i][0] = i
+            distance[0][k] = k
+
+    # Iterate over the matrix to compute the cost of deletions,insertions and/or substitutions    
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row-1] == t[col-1]:
+                cost = 0 # If the characters are the same in the two strings in a given position [i,j] then the cost is 0
+            else:
+                # In order to align the results with those of the Python Levenshtein package, if we choose to calculate the ratio
+                # the cost of a substitution is 2. If we calculate just distance, then the cost of a substitution is 1.
+                if ratio_calc == True:
+                    cost = 2
+                else:
+                    cost = 1
+            distance[row][col] = min(distance[row-1][col] + 1,      # Cost of deletions
+                                 distance[row][col-1] + 1,          # Cost of insertions
+                                 distance[row-1][col-1] + cost)     # Cost of substitutions
+    if ratio_calc == True:
+        # Computation of the Levenshtein Distance Ratio
+        Ratio = ((len(s)+len(t)) - distance[row][col]) / (len(s)+len(t))
+        return Ratio
+    else:
+        print(distance) # Uncomment if you want to see the matrix showing how the algorithm computes the cost of deletions,
+        # insertions and/or substitutions
+        # This is the minimum number of edits needed to convert string a to string b
+        return "The strings are {} edits away".format(distance[row][col])
